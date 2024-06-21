7dfe547abbe55f1c96cac9ec2a01e6e38c86dcdd
AleCaramia <126569687+AleCaramia@users.noreply.github.com>
2024-06-21 09:55:34 +0200

    Add files via upload

--- rootyPi/light/lightSens.py
+++ rootyPi/light/lightSens.py
@@ -1,10 +1,7 @@
 from pathlib import Path
-import threading
 import paho.mqtt.client as PahoMQTT
 import time
-import random
 from queue import Queue
-import datetime
 import json
 import requests
 
@@ -12,6 +9,8 @@ P = Path(__file__).parent.absolute()
 SETTINGS = P / 'settings.json'
 
 class LightSens:
+    """Light sensor simulator that listen to sunlight simulation 
+    and lamplight to publish an almost real light sensor"""
     def __init__(self, sensorId, baseTopic, plantCode):#baseTopic = "rootyPi/userId/plantId/"
         self.sensorId = sensorId
         self.sunLight = 0
@@ -40,6 +39,7 @@ class LightSens:
         
     
 def update_sensors(sensors):
+    """Updates list of sensors by check any update into catalog of plants"""
     try:
         with open(SETTINGS, "r") as fs:                
             settings = json.loads(fs.read())            
@@ -50,7 +50,6 @@ def update_sensors(sensors):
     plants = json.loads(response.text)
     for plant in plants:
         sensId = plant["plantCode"]
-        sensId
         found = 0
         for sens in sensors:
             if sens.sensorId == sensId:
@@ -62,7 +61,6 @@ def update_sensors(sensors):
     for sens in sensors:
         found = 0
         for plant in plants:
-            
             sensId = plant["plantCode"]
             if sens.sensorId == sensId:
                 found = 1
@@ -85,7 +83,7 @@ class MyPublisher:
             print("Problem in loading settings")
         self.messageBroker = self.settings["messageBroker"]
         self.port = self.settings["brokerPort"]
-		#self.messageBroker = '192.168.1.5'
+        self.qos = self.settings["qos"]
 
     def start (self):
 		#manage connection to broker
@@ -98,7 +96,7 @@ class MyPublisher:
 
     def myPublish(self, message, topic):
 		# publish a message with a certain topic
-        self._paho_mqtt.publish(topic, message, 2)
+        self._paho_mqtt.publish(topic, message, self.qos)
 
     def myOnConnect (self, paho_mqtt, userdata, flags, rc):
         print ("Connected to %s with result code: %d" % (self.messageBroker, rc))
@@ -120,14 +118,14 @@ class MySubscriber:
             print("Problem in loading settings")
         self.messageBroker = self.settings["messageBroker"]
         self.port = self.settings["brokerPort"]
-		#self.messageBroker = '192.168.1.5'
+        self.qos = self.settings["qos"]
 
     def start (self):
 		#manage connection to broker
         self._paho_mqtt.connect(self.messageBroker, self.port)
         self._paho_mqtt.loop_start()
 		# subscribe for a topic
-        self._paho_mqtt.subscribe(self.topic, 2)
+        self._paho_mqtt.subscribe(self.topic, self.qos)
 
     def stop (self):
         self._paho_mqtt.unsubscribe(self.topic)
@@ -143,52 +141,35 @@ class MySubscriber:
             self.q.put(msg)
             print ("Topic:'" + msg.topic+"', QoS: '"+str(msg.qos)+"' Message: '"+str(msg.payload) + "'")
 
-class AllSens(threading.Thread):
-
-    def __init__(self, ThreadID, name):
-        """Initialise thread widh ID and name."""
-        threading.Thread.__init__(self)
-        self.ThreadID = ThreadID
-        self.name = name
-        #load all sensors
-        self.sensors = []
-
-    def run(self):
-        """Run thread."""
-        while True:
-            update_sensors(self.sensors) #controlla se ci sono sens nuovi
-            print(len(self.sensors))
-            for sens in self.sensors: #per sensore in lista
-                while not sens.mySub.q.empty(): # se la coda non è vuota
-                    msg = sens.mySub.q.get()
-                    if msg is None:
-                        continue
-                    else:
-                        mess = json.loads(msg.payload.decode("utf-8"))
-                        event = mess["e"][0]
-                        if msg.topic.split("/")[3] == "lampLight" and not  mess["bn"].startswith("update"):
-                            print("-------lampLight")
-                            sens.artificialLight = float(event["v"])
-                        elif msg.topic.split("/")[3] == "sunlight" and not  mess["bn"].startswith("update"):
-                            print("-----------sunlight")
-                            sens.sunLight = float(event["v"])                       
-                sens.currentState = sens.artificialLight + sens.sunLight
-                print(f"current state {sens.currentState}")
-                event = {"n": "light", "u": "lux", "t": str(time.time()), "v": float(sens.currentState)}
-                out = {"bn": sens.pubTopic,"e":[event]}
-                print(out)
-                sens.myPub.myPublish(json.dumps(out), sens.pubTopic)
-                eventAlive = {"n": sens.plantCode+"/light", "u": "IP", "t": str(time.time()), "v": ""}
-                outAlive = {"bn": sens.aliveBn ,"e":[eventAlive]}
-                print("++++++++++++")
-                print("++++++++++++")
-                print( outAlive)
-                sens.myPub.myPublish(json.dumps(outAlive), sens.aliveTopic)
-                time.sleep(2)
-            time.sleep(10)
+def main ():
+    """Main function that runs forever sending simulation data"""
+    sensors = []
+    
+    while True:
+        update_sensors(sensors)
+        for sens in sensors: 
+            while not sens.mySub.q.empty(): 
+                msg = sens.mySub.q.get()
+                if msg is None:
+                    continue
+                else:
+                    mess = json.loads(msg.payload.decode("utf-8"))
+                    event = mess["e"][0]
+                    if msg.topic.split("/")[3] == "lampLight" and not  mess["bn"].startswith("update"):
+                        sens.artificialLight = float(event["v"])
+                    elif msg.topic.split("/")[3] == "sunlight" and not  mess["bn"].startswith("update"):
+                        sens.sunLight = float(event["v"])                       
+            sens.currentState = sens.artificialLight + sens.sunLight
+            print(f"current state {sens.currentState}")
+            event = {"n": "light", "u": "lux", "t": str(time.time()), "v": float(sens.currentState)}
+            out = {"bn": sens.pubTopic,"e":[event]}
+            print(out)
+            sens.myPub.myPublish(json.dumps(out), sens.pubTopic)
+            eventAlive = {"n": sens.plantCode+"/light", "u": "IP", "t": str(time.time()), "v": ""}
+            outAlive = {"bn": sens.aliveBn ,"e":[eventAlive]}
+            sens.myPub.myPublish(json.dumps(outAlive), sens.aliveTopic)
+        time.sleep(10)
 
 if __name__ == '__main__':
     
-    thredPub = AllSens(3, "AllPubs")
-    print("Starting all publishers")
-    thredPub.start()
+    main()
