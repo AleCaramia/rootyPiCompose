21d80d25ac2c14ff1dc033dba80f66dcad12ef9c
AleCaramia <126569687+AleCaramia@users.noreply.github.com>
2024-06-21 09:56:13 +0200

    Add files via upload

--- rootyPi/moisture/moistureSens.py
+++ rootyPi/moisture/moistureSens.py
@@ -2,9 +2,7 @@ from pathlib import Path
 import threading
 import paho.mqtt.client as PahoMQTT
 import time
-import random
 from queue import Queue
-import datetime
 import json
 import requests
 from requests.exceptions import HTTPError
@@ -67,7 +65,7 @@ class MyPublisher:
             print("Problem in loading settings")
         self.messageBroker = self.settings["messageBroker"]
         self.port = self.settings["brokerPort"]
-		#self.messageBroker = '192.168.1.5'
+        self.qos = self.settings["qos"]
 
     def start (self):
 		#manage connection to broker
@@ -80,7 +78,7 @@ class MyPublisher:
 
     def myPublish(self, message, topic):
 		# publish a message with a certain topic
-        self._paho_mqtt.publish(topic, message, 2)
+        self._paho_mqtt.publish(topic, message, self.qos)
 
     def myOnConnect (self, paho_mqtt, userdata, flags, rc):
         print ("Connected to %s with result code: %d" % (self.messageBroker, rc))
@@ -104,14 +102,14 @@ class MySubscriber:
             print("Problem in loading settings")
         self.messageBroker = self.settings["messageBroker"]
         self.port = self.settings["brokerPort"]
-		#self.messageBroker = '192.168.1.5'
+        self.qos = self.settings["qos"]
 
     def start (self):
 		#manage connection to broker
         self._paho_mqtt.connect(self.messageBroker, 1883)
         self._paho_mqtt.loop_start()
-	    # subscribe for a topic
-        self._paho_mqtt.subscribe(self.topic, 2)
+        # subscribe for a topic
+        self._paho_mqtt.subscribe(self.topic, self.qos)
 
     def stop (self):
         self._paho_mqtt.unsubscribe(self.topic)
@@ -138,7 +136,6 @@ def update_sensors(sensors):
     plantTypes = get_request(settings["registry_url"] + "/valid_plant_types")
     for plant in plants:
         sensId = plant["plantCode"]
-        sensId
         found = 0
         for sens in sensors:
             if sens.sensorId == sensId:
@@ -164,44 +161,34 @@ def update_sensors(sensors):
             sensors.remove(sens)
 
 
-class AllPubs(threading.Thread):
+def main():
+    sensors = []
 
-    def __init__(self, ThreadID, name):
-        """Initialise thread widh ID and name."""
-        threading.Thread.__init__(self)
-        self.ThreadID = ThreadID
-        self.name = name
-        #load all sensors
-        self.sensors = []
-
-    def run(self):
-        """Run thread."""
-        while True:
-            update_sensors(self.sensors)
-            for sens in self.sensors:
-                waterQuantity = 0
-                if not sens.mySub.q.empty():
-                    msg = sens.mySub.q.get()
-                    if msg is None:
-                        continue
-                    else:
-                        payload = json.loads(msg.payload.decode("utf-8"))
-                        waterQuantity += float(payload['e'][0]['v'])  
-                sens.currentState += waterQuantity                   
-                event = {"n": "moisture", "u": "VWC", "t": str(time.time()), "v": float(sens.currentState)}#VolumetricWaterContent
-                out = {"bn": sens.pubTopic,"e":[event]}
-                print(out)
-                sens.myPub.myPublish(json.dumps(out), sens.pubTopic)
-                eventAlive = {"n": sens.plantCode+"/moisture", "u": "IP", "t": str(time.time()), "v": ""}
-                outAlive = {"bn": sens.aliveBn ,"e":[eventAlive]}
-                print(outAlive)
-                sens.myPub.myPublish(json.dumps(outAlive), sens.aliveTopic)
-                sens.currentState -= sens.decayCoef
-                if sens.currentState<0:
-                    sens.currentState=0
-            time.sleep(10)
+    while True:
+        update_sensors(sensors)
+        for sens in sensors:
+            waterQuantity = 0
+            if not sens.mySub.q.empty():
+                msg = sens.mySub.q.get()
+                if msg is None:
+                    continue
+                else:
+                    payload = json.loads(msg.payload.decode("utf-8"))
+                    waterQuantity += float(payload['e'][0]['v'])  
+            sens.currentState += waterQuantity                   
+            event = {"n": "moisture", "u": "VWC", "t": str(time.time()), "v": float(sens.currentState)}#VolumetricWaterContent
+            out = {"bn": sens.pubTopic,"e":[event]}
+            print(out)
+            sens.myPub.myPublish(json.dumps(out), sens.pubTopic)
+            eventAlive = {"n": sens.plantCode+"/moisture", "u": "IP", "t": str(time.time()), "v": ""}
+            outAlive = {"bn": sens.aliveBn ,"e":[eventAlive]}
+            print(outAlive)
+            sens.myPub.myPublish(json.dumps(outAlive), sens.aliveTopic)
+            sens.currentState -= sens.decayCoef
+            if sens.currentState<0:
+                sens.currentState=0
+        time.sleep(10)
 
 if __name__ == '__main__':
 
-    thredPub = AllPubs(1, "AllPubs")
-    thredPub.start()
+    main()
